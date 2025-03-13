47de0575cdcc4f7e40013d6f5121f7e3dd54216c
mostafaomar7 <87546106+mostafaomar7@users.noreply.github.com>
2025-03-14 01:06:10 +0200

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://amigo.mosalam.com',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
