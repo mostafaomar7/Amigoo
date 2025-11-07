4dbdcca87fffd170f815d8cdf150ef68e52c6755
Badawy24 <abdelrahman.ahmed2410@gmail.com>
2025-11-07 18:42:37 +0200

    Fix: Add trailing slash to production imageBaseUrl to fix image display on website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://amigoapi.mosalam.com/api/v1',
   appName: 'Amigo Store',
-  imageBaseUrl: 'https://amigoapi.mosalam.com'
+  imageBaseUrl: 'https://amigoapi.mosalam.com/'
 };
